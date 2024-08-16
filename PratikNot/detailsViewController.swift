8f05934ed2df90b5bbb27ef90d8b778c4f8e70e4
MRSgkce <mursidegokce985@gmail.com>
2024-08-16 17:02:23 +0300

    klavyeden yazma işlemi yapıldı fakat yapılan yer butonun üzerini kapatabilir. o sebeple boşluğa bastığımızda klavyenin kapanmasını sağladım.

--- PratikNot/detailsViewController.swift
+++ PratikNot/detailsViewController.swift
@@ -13,23 +13,21 @@ class detailsViewController: UIViewController {
     @IBOutlet weak var isimtextfield: UITextField!
     @IBOutlet weak var fiyattextfield: UITextField!
     @IBOutlet weak var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(klavyeyiKapat))
+        view.addGestureRecognizer(gestureRecognizer)
+    }
+    @objc func klavyeyiKapat( ){
+        view.endEditing(true)
+        
     }
     
 
     @IBAction func kaydetButon(_ sender: Any) {
     }
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
