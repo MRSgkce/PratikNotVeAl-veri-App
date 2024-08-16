dd006ff3242bf00a1ab0bab933784d430e015af9
MRSgkce <mursidegokce985@gmail.com>
2024-08-16 17:22:39 +0300

    Kullanıcıya resim seçtirildi. Galeriden seçildi.

--- PratikNot/detailsViewController.swift
+++ PratikNot/detailsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class detailsViewController: UIViewController {
+class detailsViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
 
     @IBOutlet weak var bedentextfield: UITextField!
     @IBOutlet weak var isimtextfield: UITextField!
@@ -18,6 +18,25 @@ class detailsViewController: UIViewController {
         super.viewDidLoad()
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(klavyeyiKapat))
         view.addGestureRecognizer(gestureRecognizer)
+        image.isUserInteractionEnabled=true
+        
+        
+        let imageGesture=UITapGestureRecognizer(target: self, action: #selector(gorselSec))
+        image.addGestureRecognizer(imageGesture)
+
+    }
+    
+    @objc func gorselSec( ){
+        let imagepicker = UIImagePickerController()
+        imagepicker.delegate = self
+        imagepicker.sourceType = .photoLibrary
+        imagepicker.allowsEditing=true
+        present(imagepicker, animated: true, completion: nil)
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        image.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
     }
     @objc func klavyeyiKapat( ){
         view.endEditing(true)
