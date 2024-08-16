74e1b723432322b0a3af1c74dc0316da390b74c5
MRSgkce <mursidegokce985@gmail.com>
2024-08-16 16:55:33 +0300

    ilk açılan ekranda bir ekleme butonu eklendi. Butona tıklanınca bir ürün eklenecek olan detay sayfasına geçişi sağlandı. Buton kod ile eklendi

--- PratikNot/ViewController.swift
+++ PratikNot/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //ekleme butonu ekleme
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(eklemeButonuTiklandi) )
+        
+    }
+    @objc func eklemeButonuTiklandi(){
+        performSegue(withIdentifier: "todetails", sender: nil)
     }
 
 
---
+++ PratikNot/detailsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  detailsViewController.swift
+//  PratikNot
+//
+//  Created by Mürşide Gökçe on 16.08.2024.
+//
+
+import UIKit
+
+class detailsViewController: UIViewController {
+
+    @IBOutlet weak var bedentextfield: UITextField!
+    @IBOutlet weak var isimtextfield: UITextField!
+    @IBOutlet weak var fiyattextfield: UITextField!
+    @IBOutlet weak var image: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+    @IBAction func kaydetButon(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
