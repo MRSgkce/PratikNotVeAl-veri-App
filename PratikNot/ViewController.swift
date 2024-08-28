160e94d287345319208cc81d9a2f4f36a2028fa8
MRSgkce <mursidegokce985@gmail.com>
2024-08-28 21:19:16 +0300

    istenilen bilgi ve resmi kayıt edilir. Örneğin görüp beğenilen bir ürünün özellikleri

--- PratikNot/ViewController.swift
+++ PratikNot/ViewController.swift
@@ -6,21 +6,95 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     @IBOutlet weak var tableView: UITableView!
+    
+    var isimDizisi = [String]()
+    var idDizisi = [UUID]()
+    var secilenIsim=""
+    var secilenuuid : UUID?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //ekleme butonu ekleme
-        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(eklemeButonuTiklandi) )
+        tableView.delegate = self
+        tableView.dataSource = self
         
+        // Ekleme butonu ekleme
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(eklemeButonuTiklandi))
+        
+        // Verileri al
+        verileriAl()
+        //tableView.reloadData()
     }
-    @objc func eklemeButonuTiklandi(){
+    
+    @objc func eklemeButonuTiklandi() {
+        secilenIsim = ""
+        performSegue(withIdentifier: "todetails", sender: nil)
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(verileriAl), name: NSNotification.Name(rawValue: "veriGirildi"), object: nil)
+    }
+    
+    @objc func verileriAl() {
+        isimDizisi.removeAll(keepingCapacity: false)
+        idDizisi.removeAll(keepingCapacity: false)
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Alisveris")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            let sonuclar = try context.fetch(fetchRequest)
+            
+            
+            if sonuclar.count>0{
+                for sonuc in sonuclar as! [NSManagedObject] {
+                    if let isim = sonuc.value(forKey: "isim") as? String {
+                        isimDizisi.append(isim)
+                    }
+                    if let id = sonuc.value(forKey: "id") as? UUID {
+                        idDizisi.append(id)
+                    }
+                }
+                tableView.reloadData()}
+            } catch {
+                print("Hata var")
+            }
+    }
+    
+    // UITableViewDataSource metodları
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return isimDizisi.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = isimDizisi[indexPath.row]
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "todetails" {
+            let destination = segue.destination as! detailsViewController
+            destination.secilenUrunIsmi = secilenIsim
+            destination.secilenUrunId = secilenuuid
+        }
+    }
+
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        secilenIsim = isimDizisi[indexPath.row]
+        secilenuuid = idDizisi[indexPath.row] // UUID'yi set edin
         performSegue(withIdentifier: "todetails", sender: nil)
     }
 
-
 }
-
--- PratikNot/detailsViewController.swift
+++ PratikNot/detailsViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class detailsViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
 
@@ -14,8 +15,68 @@ class detailsViewController: UIViewController,UIImagePickerControllerDelegate,UI
     @IBOutlet weak var fiyattextfield: UITextField!
     @IBOutlet weak var image: UIImageView!
     
+    var secilenUrunIsmi = ""
+    var secilenUrunId : UUID?
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if secilenUrunIsmi != "" {
+            
+            if let uuidString = secilenUrunId?.uuidString{
+                
+                    print("UUID: \(uuidString)")
+                    // Fetch işlemi burada
+                
+
+                let appdelegate=UIApplication.shared.delegate as! AppDelegate
+                let context = appdelegate.persistentContainer.viewContext
+                
+                let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Alisveris")
+                fetchRequest.predicate = NSPredicate(format: "id = %@", uuidString)
+                fetchRequest.returnsObjectsAsFaults = false
+
+                
+                do{
+                    let sonuclar = try context.fetch(fetchRequest)
+                    
+                    if sonuclar.count > 0{
+                        for sonuc in sonuclar as! [NSManagedObject]{
+                            if let isim = sonuc.value(forKey: "isim") as? String{
+                                isimtextfield.text = isim
+                                print(isim)
+                            }
+                            if let fiyat = sonuc.value(forKey: "fiyat") as? Int{
+                                fiyattextfield.text = String(fiyat)
+                            }
+                            
+                            if let beden = sonuc.value(forKey: "beden") as? String{
+                                bedentextfield.text = beden
+                            }
+                            
+                            if let gorseldata = sonuc.value(forKey: "gorsel") as? Data{
+                                let imagem = UIImage(data:gorseldata)
+                                image.image=imagem
+                            }
+                            
+                        }
+                    }
+                }catch{
+                    print("hata var")
+                }
+
+
+            }
+            
+        }else{
+            isimtextfield.text = ""
+            fiyattextfield.text=""
+            bedentextfield.text=""
+        }
+        
+        
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(klavyeyiKapat))
         view.addGestureRecognizer(gestureRecognizer)
         image.isUserInteractionEnabled=true
@@ -45,6 +106,37 @@ class detailsViewController: UIViewController,UIImagePickerControllerDelegate,UI
     
 
     @IBAction func kaydetButon(_ sender: Any) {
+        
+        let appDelegate=UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let not = NSEntityDescription.insertNewObject(forEntityName: "Alisveris", into: context)
+        
+        not.setValue(isimtextfield.text!, forKey: "isim")
+        
+        not.setValue(bedentextfield.text, forKey: "beden")
+        
+        if let fiyat = Int(fiyattextfield.text!){
+            not.setValue(fiyat, forKey: "fiyat")
+        }
+        
+        not.setValue(UUID(), forKey: "id")
+        
+        let data = image.image!.jpegData(compressionQuality: 0.5)
+        
+        not.setValue(data, forKey: "gorsel")
+        
+        do{
+            try context.save()
+            print("kayıt edildi")
+        }catch{
+            print("hata var")
+        }
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "veriGirildi"), object: nil)
+        self.navigationController?.popViewController(animated: true)
+        
+        
+        
+        
     }
 
    
